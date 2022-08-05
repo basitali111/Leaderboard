4b3eb4495268118b14682373a6ca0eecaae5769e
Basit Ali <allahbadshah148@gmail.com>
2022-08-05 23:24:09 +0500

    fixing linters

---
+++ src/modules/display.js
@@ -0,0 +1,6 @@
+const list = document.querySelector('.table');
+export default async (UI) => {
+  const scoreRecord = await UI.fetchScore();
+  list.innerHTML = '';
+  scoreRecord.forEach((scoreList) => UI.addTOList(scoreList));
+};
